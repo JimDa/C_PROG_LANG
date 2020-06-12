aa2b77e3636d37ee26324840f3310f7b4494e0c6
JimDa <1085529137@qq.com>
2020-06-13 00:33:03 +0800

    ADD

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 
 void isEOF();
 
+int count_symbol();
+
 int main() {
-    isEOF();
+//    isEOF();
+//    printf("%d", EOF);
+    count_symbol();
     return 0;
 }
 
@@ -13,4 +17,22 @@ void isEOF() {
     } else {
         printf("input not equals EOF");
     }
+}
+
+int count_symbol() {
+    int tab_count = 0;
+    int blank_count = 0;
+    int chang_line = 0;
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            tab_count++;
+        } else if (c == ' ') {
+            blank_count++;
+        } else if (c == '\n') {
+            chang_line++;
+        }
+    }
+    printf("tab_count = %d,blank_count = %d,change_line_count = %d\n", tab_count, blank_count, chang_line);
+    return 0;
 }
